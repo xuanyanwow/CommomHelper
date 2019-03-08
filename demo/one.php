74034d31fdcf5594f8710dcf0e250b62b8e8a1f1
XuanYan <59419979@qq.com>
2019-03-08 14:33:22 +0800

    first

---
+++ demo/one.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC
+ * Date: 2019/3/8
+ * Time: 14:30
+ */
+
+require "../vendor/autoload.php";
+
+use Siam\Test;
+
+$test = new Test();
+$test->test();
---
+++ src/siam/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC
+ * Date: 2019/3/8
+ * Time: 14:28
+ */
+
+namespace Siam;
+
+
+class Test
+{
+    function test(){
+        echo "test";
+    }
+}
